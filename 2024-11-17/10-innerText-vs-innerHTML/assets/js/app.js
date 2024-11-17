3a88562df2cb6c87fda0121ac23277dacb799b8e
shaharsol <shahar@gnift.com>
2024-11-17 15:57:11 +0200

    it is windows

---
+++ 2024-11-17/10-innerText-vs-innerHTML/assets/js/app.js
@@ -0,0 +1,7 @@
+function changeInnerText() {
+    document.getElementById('myParagraph1').innerText = document.getElementById('textInput1').value
+}
+
+function changeInnerHTML() {
+    document.getElementById('myParagraph2').innerHTML = document.getElementById('textInput2').value
+}
