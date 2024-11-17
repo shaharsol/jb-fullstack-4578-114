f59a4ddac61866e1b0fef96756888fa02b87f3b9
shaharsol <shahar@gnift.com>
2024-11-17 10:38:58 +0200

    7boom

--- 2024-11-17/04-7boom/assets/js/app.js
+++ 2024-11-17/04-7boom/assets/js/app.js
@@ -1,4 +1,28 @@
 // functions
+function getInputFromUser() {
+    return +prompt('enter number')
+}
+
+function userStillWantsToContinue(input) {
+    return input !== 0
+}
+
+function currentNumberDivide7(num) {
+    return num % 7 === 0
+}
+
+function currentNumberContains7(num) {
+    return num.toString().includes('7')
+}
+
+function boom() {
+    console.log('boom')
+}
+
+function notBoom(num) {
+    console.log(num)
+}
+
 // main
 
 let input = getInputFromUser()
@@ -6,7 +30,7 @@ let input = getInputFromUser()
 while(userStillWantsToContinue(input)) {
     for(let i = 0; i < input; i++) {
         if(currentNumberDivide7(i) || currentNumberContains7(i)) {
-            boom(i)
+            boom()
         } else {
             notBoom(i)
         }
