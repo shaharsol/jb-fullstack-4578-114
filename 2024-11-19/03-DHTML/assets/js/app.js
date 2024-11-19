52064029c0ec720091efe081f9f6688ab9744475
shaharsol <shahar@gnift.com>
2024-11-19 13:10:27 +0200

    alota

--- 2024-11-19/03-DHTML/assets/js/app.js
+++ 2024-11-19/03-DHTML/assets/js/app.js
@@ -32,3 +32,23 @@ function populateImages() {
 
 
 }
+
+
+function addFruit() {
+    const container = document.getElementById('fruits')
+    const fruit = prompt('please enter a fruit')
+    const newHTML = `<li>${fruit}</li>`
+    container.innerHTML += newHTML
+}
+
+function addCar() {
+    // collect data
+    const container = document.getElementById('cars')
+    const car = document.getElementById('car').value
+
+    // generate new HTML from the collected data
+    const newHTML = `<li>${car}</li>`
+
+    // render the new html to the DOM
+    container.innerHTML += newHTML
+}
