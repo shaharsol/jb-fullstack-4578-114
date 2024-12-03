8d3d10e666162cebf1b90f0ce008a5a893a9e766
shaharsol <shahar@gnift.com>
2024-12-03 14:43:00 +0200

    grades exercise

---
+++ 2024-12-03-exceptions/13-iife/assets/js/app.js
@@ -0,0 +1,38 @@
+// const message = 'hello world'
+// console.log(message)
+// console.log(message[1])
+// message[1] = 'o'
+// console.log(message)
+
+// let counter = 3
+// console.log(counter)
+// count = 4
+// alert = message => console.log(message)
+// console.log(counter)
+
+
+// alert(window.count)
+// alert('shouyld be alerted but we see it in console insted')
+
+
+"use strict";
+
+const message = 'hello world'
+console.log(message)
+console.log(message[1])
+// message[1] = 'o'
+console.log(message)
+
+let counter = 3
+console.log(counter)
+// count = 4
+alert = message => console.log(message)
+window.alert('hello beery')
+console.log(counter)
+
+
+alert(window.count)
+alert('shouyld be alerted but we see it in console insted')
+
+
+
