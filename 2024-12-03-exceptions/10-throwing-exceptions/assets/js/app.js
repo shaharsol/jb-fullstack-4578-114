94035140c8c3c959bd022becae05d19e7756680d
shaharsol <shahar@gnift.com>
2024-12-03 13:41:03 +0200

    vat

--- 2024-12-03-exceptions/10-throwing-exceptions/assets/js/app.js
+++ 2024-12-03-exceptions/10-throwing-exceptions/assets/js/app.js
@@ -1,7 +1,16 @@
 const showVat = (event) => {
     event.preventDefault()
-    const vat = getVat(document.getElementById('price').value)
-    console.log(`the vat for ${document.getElementById('price').value} is ${vat}`)
+    const price = +document.getElementById('price').value
+    try {
+        const vat = getVat(price)
+        console.log(`the vat for ${price} is ${vat}`)
+    } catch (e) {
+        alert(e.message)
+    }
 }
 
-const getVat = (price) => price * 0.17
+const getVat = (price) => { 
+    if (!price) throw new Error('price is required')
+    if (price < 0) throw new Error('price must be a positive number')
+    return price * 0.17 
+}
