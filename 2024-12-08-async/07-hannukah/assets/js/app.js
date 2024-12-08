71eb5cda65185f844b797fcb2c95fe7e5c12f863
shaharsol <shahar@gnift.com>
2024-12-08 13:10:17 +0200

    hanukah

---
+++ 2024-12-08-async/07-hannukah/assets/js/app.js
@@ -0,0 +1,15 @@
+"use strict";
+
+(() => {
+
+    const getNumberOfCandles = (dayNumber, successCallback, errorCallback) => {
+        setTimeout(() => {
+            if (dayNumber < 1) errorCallback('dayNumber must be greater than 0');
+            if (dayNumber > 8) errorCallback('no Isru Chag in Hanukah');
+            successCallback(dayNumber + 1)
+        }, 3000)
+    }
+
+    // use 
+
+})()
