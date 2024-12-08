862ba72c7f28d1da289a9e84deb230b014c51e4e
shaharsol <shahar@gnift.com>
2024-12-08 09:50:43 +0200

    alota

---
+++ 2024-12-08-async/02-sync-function/assets/js/app.js
@@ -0,0 +1,39 @@
+"use strict";
+
+(() => {
+
+    const user = {
+        id: 123,
+        name: 'Israel Israeli'
+    }
+
+    ////////////////////////////////////////////////////////////
+    const showUser = () => {
+        console.log('function started')
+        console.log(user)
+        console.log('function finished')
+    }
+
+
+    document.getElementById('showUser').addEventListener('click', () => {
+        console.log('event started')
+        showUser()
+        console.log('event finished')
+    })
+
+    //////////////////////////////////////////////////////////////
+    const getUser = () => {
+        console.log('function started')
+        console.log('function finished')
+        return user
+    }
+
+    document.getElementById('getUser').addEventListener('click', () => {
+        console.log('event started')
+        const user = getUser()
+        console.log(user)
+        console.log('event finished')
+    })
+
+
+})()
