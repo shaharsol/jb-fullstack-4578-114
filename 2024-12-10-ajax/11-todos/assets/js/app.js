e3375228c0c929efe7cea23e1e051d0d4d3fa8c3
shaharsol <shahar@gnift.com>
2024-12-10 14:19:45 +0200

    total todos

--- 2024-12-10-ajax/11-todos/assets/js/app.js
+++ 2024-12-10-ajax/11-todos/assets/js/app.js
@@ -9,14 +9,24 @@
         return fetch(url).then(response => response.json())
     }
 
-    const generateHTML = photos => {
-        const newHTML = photos
-            .map(photo => {
-                const { title, thumbnailUrl } = photo // deconstruction
+    const generateStatsTable = todos => {
+        return `
+            <tr>
+                <td>total todos:</td>
+                <td>${todos.length}</td>
+            </tr>
+        `
+    }
+
+    const generateTodosTable = todos => {
+        const newHTML = todos
+            .map(todo => {
+                const { userId, title, completed } = todo // deconstruction
                 return `
                     <tr>
+                        <td>${userId}</td>
                         <td>${title}</td>
-                        <td><img src="${thumbnailUrl}"></td>
+                        <td>${completed ? 'Yes' : 'No'}</td>
                     </tr>
                 `
             })
@@ -24,19 +34,23 @@
         return newHTML
     }
 
-    const renderHTML = newHTML => document.getElementById('photos').innerHTML = newHTML
+    const renderTodosTable = newHTML => document.getElementById('todos').innerHTML = newHTML
+
+    const renderStatsTable = newHTML => document.getElementById('stats').innerHTML = newHTML
 
     document.getElementById('button').addEventListener('click', async () => {
         try {
 
             // get data
-            const photos = await getData('https://jsonplaceholder.typicode.com/photos')
+            const todos = await getData('https://jsonplaceholder.typicode.com/todos')
 
             // generate html
-            const newHTML = generateHTML(photos)
+            const todosTableHTML = generateTodosTable(todos)
+            const statsTableHTML = generateStatsTable(todos)
 
             // render html
-            renderHTML(newHTML)
+            renderTodosTable(todosTableHTML)
+            renderStatsTable(statsTableHTML)
             
         } catch (e) {
             console.warn(e)
