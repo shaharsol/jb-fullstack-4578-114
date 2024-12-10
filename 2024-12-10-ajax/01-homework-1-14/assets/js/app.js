8b3e1a853b3dc4aca94028adbaaa60bdf46548a7
shaharsol <shahar@gnift.com>
2024-12-10 09:47:06 +0200

    hw final

--- 2024-12-10-ajax/01-homework-1-14/assets/js/app.js
+++ 2024-12-10-ajax/01-homework-1-14/assets/js/app.js
@@ -2,14 +2,34 @@
 
 (() => {
 
-    document.getElementById('button').addEventListener('click', () => {
-        navigator.geolocation.getCurrentPosition(position => {
-            document.getElementById('location').innerHTML = `
-                latitude: ${position.coords.latitude}, longitude: ${position.coords.longitude}
-            `
-        }, error => {
-            document.getElementById('location').innerHTML = error.message
+    const getPizzaFromServerAsync = () => {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                const randomNumber = Math.floor(Math.random() * 100)
+                if (randomNumber % 2 === 0) {
+                    return resolve('here is your pizza')
+                    // resolve is actually what we added as callback in then.
+                    // so if we did .then(console.log), then resolve is now console.log
+                    // so instead of resolve('here is your pizza') javascript invokes
+                    // return console.log('here is your pizza')
+                }
+                return reject('no pizza today')
+            }, 1000)
         })
+    }
+
+    const displayPizza = pizza => console.log(pizza)
+
+    document.getElementById('button').addEventListener('click', async () => {
+        // getPizzaFromServerAsync()
+        //     .then(console.log)
+        //     .catch(console.warn)
+        try {
+            const pizza = await getPizzaFromServerAsync()
+            console.log(pizza)
+        } catch (e) {
+            console.warn(e)
+        }
     })
 
 })()
