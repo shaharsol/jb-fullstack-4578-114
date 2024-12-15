4f9547667124c51e7edce381faf6736bde9c781d
shaharsol <shahar@gnift.com>
2024-12-15 12:03:26 +0200

    sorted

--- 2024-12-15-jquery-complexity/05-array-search/assets/js/app.js
+++ 2024-12-15-jquery-complexity/05-array-search/assets/js/app.js
@@ -2,7 +2,18 @@
 
 (() => {
 
-    const grades = [100, 99, 78, 87, 65, 66, 64, 99, 92, 80]
+    // a function to find an item in an un-sorted array
+    // O is O(n)
+    const isGradeInGrades = grades => grades.find(grade => grade === input) ? 'yes' : 'no'
+    
+
+    const isGradeInGradesSorted = grades => {}
+
+    const grades =  [100, 99, 78, 87, 65, 66, 64, 99, 92, 80]
+    const grades2 = [64, 65, 66, 78, 80, 87, 92, 99, 99, 100]
+
 
     const input = +prompt('enter a number to check if it is in the array')
+
+    console.log(isGradeInGrades(grades))
 })()
