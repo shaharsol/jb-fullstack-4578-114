dc2d5b72ad90274843d01783543a462eecbe31b2
shaharsol <shahar@gnift.com>
2024-12-31 09:55:39 +0200

    singleton

--- 2024-12-31-oop2/01-stateful-vs-stateless/src/animals-service.ts
+++ 2024-12-31-oop2/01-stateful-vs-stateless/src/animals-service.ts
@@ -1,9 +1,13 @@
 import Animal from "./animal.js";
 
 export default class AnimalService {
-
+    static instance = new AnimalService()
+    static getInstance() {
+        return this.instance
+    }
     // this is a stateless class
     // it doesn't hold ANY data
+    private constructor () {}
 
     getAnimalsFromDatabase(): Animal[] {
         // ... fetches animals from database
--- 2024-12-31-oop2/01-stateful-vs-stateless/src/app.ts
+++ 2024-12-31-oop2/01-stateful-vs-stateless/src/app.ts
@@ -1,7 +1,9 @@
 import AnimalService from "./animals-service.js";
 
-const animalService = new AnimalService()
+// const animalService = new AnimalService()
+const animalService = AnimalService.getInstance()
 const animals = animalService.getAnimalsFromDatabase()
-const animalService2 = new AnimalService()
+// const animalService2 = new AnimalService()
+const animalService2 = AnimalService.getInstance()
 
 animalService2.getAnimalsFromDatabase()
