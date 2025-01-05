43fd7a9e5ce3c1de576f2d245b0ff1d98e9bae67
shaharsol <shahar@gnift.com>
2025-01-05 13:39:32 +0200

    Ginger added

--- 2025-01-05-react/02-components/src/components/cat-list/CatList.tsx
+++ 2025-01-05-react/02-components/src/components/cat-list/CatList.tsx
@@ -1,10 +1,12 @@
 import Eden from '../cats/eden/Eden'
+import Ginger from '../cats/ginger/Ginger'
 import './CatList.css'
 
 export default function CatList(): JSX.Element {
     return (
         <div className='CatList'>
            <Eden />
+           <Ginger />
         </div>
     )
 }
---
+++ 2025-01-05-react/02-components/src/components/cats/ginger/Ginger.tsx
@@ -0,0 +1,13 @@
+import './Ginger.css'
+
+export default function Ginger(): JSX.Element {
+    return (
+        <div className='Ginger'>
+            <div>Ginger</div>
+            <div>Age: 6</div>
+            <div>
+                <img src="ginger.avif" />
+            </div>
+        </div>
+    )
+}
