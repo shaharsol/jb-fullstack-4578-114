6fefe36c2811883e1917c9b2b7bce0350ef17abe
shaharsol <shahar@gnift.com>
2025-01-05 14:33:46 +0200

    rendering lists

--- 2025-01-05-react/03-embedding/src/components/demo/Demo.tsx
+++ 2025-01-05-react/03-embedding/src/components/demo/Demo.tsx
@@ -1,9 +1,51 @@
 import './Demo.css'
 
 export default function Demo(): JSX.Element {
+
+    // primitives
+    const name = 'Kfir'
+    const age = 22
+    const isAdult = age > 18
+    const grades = [70, 80, 90]
+
+    const dogs = [
+        {
+            name: 'cheif',
+            age: 6
+        },
+        {
+            name: 'noop',
+            age: 4
+        }
+    ]
+
+    function sayHi() {
+        alert('hi')
+    }
+
     return (
         <div className='Demo'>
-            <p>hello demo</p>
+            <p>hello {name}</p>
+            <p>you are {age} years old</p>
+            <p>you are an {isAdult ? 'adult' : 'child'}</p>
+            {/* <p>your grades are {grades}</p> */}
+            <p>your grades are:</p>
+            <ul>
+                {grades.map((grade, index) => <li key={index}>{grade}</li>)}
+            </ul>
+            <button onClick={sayHi}>say hi</button>
+            <p>dogs:</p>
+            <table>
+                <thead>
+
+                </thead>
+                <tbody>
+                    {dogs.map(dog => <tr>
+                        <td>{dog.name}</td>
+                        <td>{dog.age}</td>
+                    </tr>)}
+                </tbody>
+            </table>
         </div>
     )
 }
