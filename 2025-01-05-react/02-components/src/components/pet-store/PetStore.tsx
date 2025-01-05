db7d3cebc713a1d9eea29bbbc69f72ff769030ef
shaharsol <shahar@gnift.com>
2025-01-05 12:04:12 +0200

    components

---
+++ 2025-01-05-react/02-components/src/components/pet-store/PetStore.tsx
@@ -0,0 +1,10 @@
+import './PetStore.css'
+
+export default function PetStore() {
+    return (
+        <div className="PetStore">
+            <p>hello petstore</p>
+        </div>
+    )
+    
+}
---
+++ 2025-01-05-react/02-components/src/main.tsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import PetStore from './components/pet-store/PetStore'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <p>welcome to my website</p>
+    <PetStore />
+  </StrictMode>,
+)
