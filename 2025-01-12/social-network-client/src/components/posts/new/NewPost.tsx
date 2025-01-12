7020ff8c3bdac094967a1e4cbb50f979993d0e64
danny.teller <danny.teller.io@gmail.com>
2025-01-12 14:49:07 +0200

    new post

--- 2025-01-12/social-network-client/src/components/posts/new/NewPost.tsx
+++ 2025-01-12/social-network-client/src/components/posts/new/NewPost.tsx
@@ -9,7 +9,7 @@ interface NewPostProps {
 }
 export default function NewPost(props: NewPostProps): JSX.Element {
 
-    const { register, handleSubmit, reset } = useForm<PostDraft>()
+    const { register, handleSubmit, reset, formState } = useForm<PostDraft>()
 
     async function submit(draft: PostDraft) {
         try {
@@ -24,8 +24,28 @@ export default function NewPost(props: NewPostProps): JSX.Element {
     return (
         <div className='NewPost'>
             <form onSubmit={handleSubmit(submit)}>
-                <input placeholder='title' {...register('title')}/>
-                <textarea placeholder='post body' {...register('body')} />
+                <input placeholder='title' {...register('title', {
+                    required: {
+                        value: true,
+                        message: 'you must provide a title'
+                    },
+                    minLength: {
+                        value: 10,
+                        message: 'title must be 10 chars long'
+                    }
+                })}/>
+                <span className='error'>{formState.errors.title?.message}</span>
+                <textarea placeholder='post body' {...register('body', {
+                    required: {
+                        value: true,
+                        message: 'you must provide a body'
+                    },
+                    minLength: {
+                        value: 20,
+                        message: 'body must be 10 chars long'
+                    },
+                })} />
+                <span className='error'>{formState.errors.body?.message}</span>
                 <button>Add Post</button>
             </form>
         </div>
