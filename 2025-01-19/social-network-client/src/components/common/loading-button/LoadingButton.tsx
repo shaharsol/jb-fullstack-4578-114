b2063d91d8a2e0fbc6fcbee75f55e45d623590d5
shaharsol <shahar@gnift.com>
2025-01-19 09:39:38 +0200

    unfollow button

--- 2025-01-19/social-network-client/src/components/common/loading-button/LoadingButton.tsx
+++ 2025-01-19/social-network-client/src/components/common/loading-button/LoadingButton.tsx
@@ -4,19 +4,21 @@ import loadingImageSource from '../../../assets/images/loading.webp'
 interface LoadingButtonProps {
     isSubmitting: boolean,
     buttonText: string,
-    loadingText: string
+    loadingText: string,
+    onClick?: React.MouseEventHandler<HTMLButtonElement>
 }
 export default function LoadingButton(props: LoadingButtonProps): JSX.Element {
 
     const {
         isSubmitting,
         buttonText,
-        loadingText
+        loadingText,
+        onClick,
     } = props
 
     return (
         <div className='LoadingButton'>
-            {!isSubmitting && <button>{buttonText}</button>}
+            {!isSubmitting && <button onClick={onClick}>{buttonText}</button>}
             {isSubmitting && <p>{loadingText}... <i><img src={loadingImageSource}/></i></p>}
         </div>
     )
--- 2025-01-19/social-network-client/src/components/follows/follow/Follow.tsx
+++ 2025-01-19/social-network-client/src/components/follows/follow/Follow.tsx
@@ -2,6 +2,8 @@ import User from '../../../models/user/User'
 import './Follow.css'
 import profilePicSource from '../../../assets/images/profile.jpg'
 import followingService from '../../../services/following'
+import LoadingButton from '../../common/loading-button/LoadingButton'
+import { useState } from 'react'
 
 interface FollowProps {
     user: User
@@ -12,13 +14,18 @@ export default function Follow(props: FollowProps): JSX.Element {
             
     const { user: {id, name}, removeFromFollowingList } = props
 
+    const [isSubmitting, setIsSubmitting] = useState<boolean>(false)
+
     async function unfollow() {
         if(window.confirm(`are you sure you wanna stop following ${name}?`)) {
             try {
+                setIsSubmitting(true)
                 await followingService.unfollow(id)
                 removeFromFollowingList(id)
             } catch (e) {
                 alert(e)
+            } finally {
+                setIsSubmitting(false)
             }
         }
     }
@@ -33,6 +40,12 @@ export default function Follow(props: FollowProps): JSX.Element {
             </div>
             <div>
                 <button onClick={unfollow}>unfollow</button>
+                <LoadingButton 
+                    onClick={unfollow}
+                    isSubmitting={isSubmitting}
+                    buttonText='Unfollow'
+                    loadingText='Unfollowing'
+                />
             </div>
         </div>
     )
