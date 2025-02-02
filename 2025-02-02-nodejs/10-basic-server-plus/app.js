75db604eaec35aefc38660602534c0aa8222082a
shaharsol <shahar@gnift.com>
2025-02-02 14:10:25 +0200

    server became a swicth case

--- 2025-02-02-nodejs/10-basic-server-plus/app.js
+++ 2025-02-02-nodejs/10-basic-server-plus/app.js
@@ -1,5 +1,33 @@
 const { createServer } = require('http')
 
-const requestListener = (request, response) => response.end('hello from server')
+const user = {
+    name: 'Yuval',
+    email: 'yuval@gmail.com',
+    age: 23
+}
+
+const list = `
+name,age,phone
+ido,23,55555
+mishel,23,44444
+daniel,32,33333
+`
+
+const requestListener = (request, response) => {
+
+    switch (request.url) {
+        case '/user':
+            response.setHeader('Content-Type', 'application/json')
+            response.end(JSON.stringify(user))
+            break;
+        case '/list':
+            response.setHeader('Content-Type', 'text/csv')
+            response.end(list)
+            break;
+        default:
+            response.end('unknown operation')
+            break;
+    }
+}
 
 createServer(requestListener).listen(3000, () => console.log('server started on port 3000...'))
