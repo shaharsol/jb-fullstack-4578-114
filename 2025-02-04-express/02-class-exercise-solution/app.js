dcce039b2eea279c7ff3071ba2730d725376cab0
shaharsol <shahar@gnift.com>
2025-02-04 11:20:45 +0200

    exercise finished

--- 2025-02-04-express/02-class-exercise-solution/app.js
+++ 2025-02-04-express/02-class-exercise-solution/app.js
@@ -5,35 +5,36 @@ const { toXML } = require('jstoxml')
 const server = express()
 
 // naive way...
-const getUsers = async (req, res, next ) => {
-    // getData from server
-    const response = await axios('https://jsonplaceholder.typicode.com/users')
-    let users = response.data
+// const getUsers = async (req, res, next ) => {
+//     // getData from server
+//     const response = await axios('https://jsonplaceholder.typicode.com/users')
+//     let users = response.data
     
-    // filter users if required
-    if (req.query.search) {
-        users = users.filter(({name}) => name.includes(req.query.search))
-    }
+//     // filter users if required
+//     if (req.query.search) {
+//         users = users.filter(({name}) => name.includes(req.query.search))
+//     }
 
-    // convert to XML if required
-    if (req.query.format === 'xml') {
-        res.setHeader('Content-type', 'text/xml')
-        return res.send(toXML(users))
-    } 
-    res.json(users)
-}
+//     // convert to XML if required
+//     if (req.query.format === 'xml') {
+//         res.setHeader('Content-type', 'text/xml')
+//         return res.send(toXML(users))
+//     } 
+//     res.json(users)
+// }
 
 // server.get('/users', getUsers)
 
-const getData = async (req, res, next) => {
+// modular way...
+const getUsers = async (req, res, next) => {
     const response = await axios('https://jsonplaceholder.typicode.com/users')
-    req.users = response.data
+    req.data = response.data
     next()
 }
 
 const filterData = (req, res, next) => {
     if (req.query.search) {
-        req.users = req.users.filter(({name}) => name.includes(req.query.search))
+        req.data = req.data.filter(({name}) => name.includes(req.query.search))
     }
     next()
 }
@@ -41,11 +42,11 @@ const filterData = (req, res, next) => {
 const respond = (req, res, next) => {
     if (req.query.format === 'xml') {
         res.setHeader('Content-type', 'text/xml')
-        return res.send(toXML(req.users))
+        return res.send(toXML(req.data))
     } 
-    res.json(req.users)
+    res.json(req.data)
 }
 
-server.get('/users', getData, filterData, respond)
+server.get('/users', getUsers, filterData, respond)
 
 server.listen(3000, () => console.log('started'))
