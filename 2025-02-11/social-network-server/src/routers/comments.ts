18e19637e7c79eccb3541bbedd268f6f3b00b287
shaharsol <shahar@gnift.com>
2025-02-11 11:06:35 +0200

    comments post working double validations

--- 2025-02-11/social-network-server/src/routers/comments.ts
+++ 2025-02-11/social-network-server/src/routers/comments.ts
@@ -1,5 +1,10 @@
 import { Router } from "express";
+import validation from "../middlewares/validation";
+import { newCommentParamsValidator, newCommentValidator } from "../controllers/comments/validator";
+import { createComment } from "../controllers/comments/controller";
+import paramsValidation from "../middlewares/params-validation";
 
 const router = Router()
 
+router.post('/:postId', validation(newCommentValidator), paramsValidation(newCommentParamsValidator), createComment)
 export default router
