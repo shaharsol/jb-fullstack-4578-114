fbac1fd97cb33311d47aa3f45bcceb6687f2e714
shaharsol <shahar@gnift.com>
2025-02-11 12:10:37 +0200

    raw queries

--- 2025-02-11/social-network-server/src/controllers/feed/controller.ts
+++ 2025-02-11/social-network-server/src/controllers/feed/controller.ts
@@ -6,32 +6,39 @@ import sequelize from "../../db/sequelize";
 
 export async function getFeed(req: Request, res: Response, next: NextFunction) {
     try {
-        // const userId = '1230ae30-dc4f-4752-bd84-092956f5c633'
+        const userId = '1230ae30-dc4f-4752-bd84-092956f5c633'
 
-        // const user = await User.findByPk(userId, {
-        //     include: [ { 
-        //         model: User,
-        //         as: 'following',
-        //         include: [ { 
-        //             model: Post,
-        //             ...postIncludes
-        //         } ]
-        //     } ]
-        // })
+        const user = await User.findByPk(userId, {
+            include: [ { 
+                model: User,
+                as: 'following',
+                include: [ { 
+                    model: Post,
+                    ...postIncludes
+                } ]
+            } ]
+        })
 
-        // const feed = user.following.reduce((cummulative: Post[], { posts }) => {
-        //     return [...cummulative, ...posts]
-        // }, [])
+        const feed = user.following.reduce((cummulative: Post[], { posts }) => {
+            return [...cummulative, ...posts]
+        }, [])
 
-        // res.json(feed)        
+        res.json(feed)        
 
-        const result = sequelize.query(`
-            select	id, title
-            from 	posts
-            JOIN	follows on posts.user_id = follows.followee_id
-            AND		follows.follower_id = '1230ae30-dc4f-4752-bd84-092956f5c633'
-        `)        
+        // example how to do the same with RAW QUERY using sequelize:
+        // const feed = await sequelize.query(`
+        //     select	posts.*
+        //     from 	posts
+        //     JOIN	follows on posts.user_id = follows.followee_id
+        //     AND		follows.follower_id = ?
+        // `, {
+        //     replacements: [ userId ],
+        //     model: Post
+        // })        
 
+        // await Promise.all(feed.map(post => post.reload({...postIncludes})))
+
+        // res.json(feed)
     } catch (e) {
         next(e)
     }
