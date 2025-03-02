61e3d809363a41853241ff67b8d16d9ad1432bf5
shaharsol <shahar@gnift.com>
2025-03-02 15:54:09 +0200

    upload works

--- 2025-03-02/02-file-uploads/backend/src/middlewares/file-uploader.ts
+++ 2025-03-02/02-file-uploads/backend/src/middlewares/file-uploader.ts
@@ -1,4 +1,7 @@
+import { CreateBucketCommand, S3Client } from "@aws-sdk/client-s3";
+import { Upload } from "@aws-sdk/lib-storage";
 import { NextFunction, Request, Response } from "express";
+import { UploadedFile } from "express-fileupload";
 
 declare global {
     namespace Express {
@@ -8,11 +11,46 @@ declare global {
     }
 }
 
-export default function fileUploader (req: Request, res: Response, next: NextFunction) {
+export default async function fileUploader (req: Request, res: Response, next: NextFunction) {
     if (!req.files) return next()
 
     // handle actual file upload
+    const s3Client = new S3Client({
+        region: 'us-east-1',
+        forcePathStyle: true, // required only for localstack
+        credentials: {
+            accessKeyId: 'test',
+            secretAccessKey: 'test'
+        },
+        endpoint: 'http://localhost:4566'
+    })
+
+    const postImage = req.files.postImage as UploadedFile
+
+    try {
+        await s3Client.send(
+            new CreateBucketCommand({
+                Bucket: 'il.co.johnbryce.shaharsol'
+            })
+        )
+    } catch (e) {
+        // ignore
+        console.log('Bucket probably already exist')
+    }
+
+
+    const upload = new Upload({
+        client: s3Client,
+        params: {
+            Bucket: 'il.co.johnbryce.shaharsol',
+            Key: postImage.name,
+            Body: postImage.data
+        }
+    })
+
+    const response = await upload.done()
     
-    req.imageUrl = 'https://cdn.britannica.com/55/174255-050-526314B6/brown-Guernsey-cow.jpg'
+    // req.imageUrl = 'https://cdn.britannica.com/55/174255-050-526314B6/brown-Guernsey-cow.jpg'
+    req.imageUrl = response.Location
     next()
 }
