7298e1506bdc9e35be709e131e68ff0997c657b2
shaharsol <shahar@gnift.com>
2025-03-18 10:53:25 +0200

    alota

--- 2025-03-18-aws/04-tests/frontend/src/components/io/Io.tsx
+++ 2025-03-18-aws/04-tests/frontend/src/components/io/Io.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-case-declarations */
 import { createContext, PropsWithChildren, useEffect, useState } from "react";
 import { io } from "socket.io-client";
 import { useAppDispatch } from "../../redux/hooks";
@@ -6,11 +7,14 @@ import Post from "../../models/post/Post";
 import { v4 } from "uuid";
 import SocketMessages from "socket-enums-shaharsol";
 import Comment from "../../models/comment/Comment";
+import useUserId from "../../hooks/useUserId";
+// import { setNewContent } from "../../redux/feedSlice";
 
 interface SocketContextInterface {
     xClientId: string
 }
 
+// eslint-disable-next-line react-refresh/only-export-components
 export const SocketContext = createContext<SocketContextInterface>({
     xClientId: ''
 })
@@ -39,7 +43,16 @@ export default function Io(props: PropsWithChildren): JSX.Element {
                 switch(eventName) {
                     case SocketMessages.NEW_POST : 
                         const newPostPayload = payload.data as Post
-                        dispatch(newPost(newPostPayload))
+                        // eslint-disable-next-line react-hooks/rules-of-hooks
+                        // const isFollowing = useAppSelector(state => state.following.following.findIndex(f => f.id === newPostPayload.userId) > -1)
+
+                        // eslint-disable-next-line react-hooks/rules-of-hooks
+                        if (newPostPayload.userId === useUserId()) {
+                            dispatch(newPost(newPostPayload))
+                        }
+                        // if (isFollowing) {
+                        //     dispatch(setNewContent(true))
+                        // } 
                         break;
                     case SocketMessages.NEW_COMMENT:
                         const newCommentPayload = payload.data as Comment
