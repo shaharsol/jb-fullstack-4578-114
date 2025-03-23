997d6af67e73c890d4d0271ec82f2c56896465b2
shaharsol <shahar@gnift.com>
2025-03-23 12:08:56 +0200

    worker wip

--- 2025-03-23-aws2/05-aws/image-resizer/src/worker.ts
+++ 2025-03-23-aws2/05-aws/image-resizer/src/worker.ts
@@ -1,19 +1,54 @@
-import { ReceiveMessageCommand, SQSClient } from '@aws-sdk/client-sqs'
+import { GetObjectCommand, S3Client } from '@aws-sdk/client-s3'
+import { DeleteMessageCommand, ReceiveMessageCommand, SQSClient } from '@aws-sdk/client-sqs'
 import config from 'config'
 
 const sqsConfig = JSON.parse(JSON.stringify(config.get('sqs.connection')))
+const s3Config = JSON.parse(JSON.stringify(config.get('s3.connection')))
 
 if (!config.get<boolean>('sqs.isLocalstack')) delete sqsConfig.endpoint
+if (!config.get<boolean>('s3.isLocalstack')) delete s3Config.endpoint
 
 const sqsClient = new SQSClient(sqsConfig)
+const s3Client = new S3Client(s3Config)
 
 async function work() {
     // while(true) {
-        const receivedMessage = await sqsClient.send(new ReceiveMessageCommand({
+        const { Messages } = await sqsClient.send(new ReceiveMessageCommand({
             QueueUrl: config.get<string>('sqs.queueUrl'),
             MaxNumberOfMessages: 1
         }))
-        console.log(receivedMessage)
+        console.log(Messages)
+        if (Messages) {
+            const { Body, ReceiptHandle } = Messages[0]
+
+            const message = JSON.parse(Body!)
+
+            console.log(message)
+
+            const s3Response = await s3Client.send(new GetObjectCommand({
+                Bucket: message.bucket,
+                Key: message.key
+            }))
+
+            console.log(s3Response)
+
+            const photoContent = await s3Response.Body?.transformToByteArray()
+
+            console.log(photoContent)
+
+            // here we process the image
+            // let's say we're done
+
+            await sqsClient.send(new DeleteMessageCommand({
+                QueueUrl: config.get<string>('sqs.queueUrl'),
+                ReceiptHandle,
+            }))            
+
+        } else {
+            console.log('nothing to process....')
+        }
+
+
     // }
 }
 
