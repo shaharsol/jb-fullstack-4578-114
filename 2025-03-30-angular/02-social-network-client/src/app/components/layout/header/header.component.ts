b362466382cb23dabb6a8dbc9ab6670c638ce8e8
shaharsol <shahar@gnift.com>
2025-03-30 12:17:07 +0300

    routing

--- 2025-03-30-angular/02-social-network-client/src/app/components/layout/header/header.component.ts
+++ 2025-03-30-angular/02-social-network-client/src/app/components/layout/header/header.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
