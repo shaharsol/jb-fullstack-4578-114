74b242f6d1592f0e637f04708f9229881e5d371e
shaharsol <shahar@gnift.com>
2025-03-30 14:53:56 +0300

    for empty

---
+++ 2025-03-30-angular/02-social-network-client/src/app/components/posts/post/post.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post',
+  imports: [],
+  templateUrl: './post.component.html',
+  styleUrl: './post.component.css'
+})
+export class PostComponent {
+
+}
--- 2025-03-30-angular/02-social-network-client/src/app/components/posts/profile/profile.component.ts
+++ 2025-03-30-angular/02-social-network-client/src/app/components/posts/profile/profile.component.ts
@@ -3,16 +3,20 @@ import { Component, computed, OnDestroy, OnInit, signal } from '@angular/core';
 import { AllcapsPipe } from '../../../pipes/allcaps.pipe';
 import { FormsModule } from '@angular/forms';
 import { ProfileService } from '../../../services/profile.service';
+import { Post } from '../../../models/post/post.model';
+import { PostComponent } from "../post/post.component";
 
 @Component({
   selector: 'app-profile',
-  imports: [],
+  imports: [PostComponent],
 //   providers: [ProfileService], // if the components needs its own ProfileService, 
 // it states here like so...
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent implements OnInit, OnDestroy {
+
+    profile = signal<Post[]>([])
 
     constructor (
         public profileService: ProfileService
@@ -22,8 +26,8 @@ export class ProfileComponent implements OnInit, OnDestroy {
     }
     
     async ngOnInit(): Promise<void> {
-        const profile = await this.profileService.getProfile()
-        console.log(profile)
+        const profilePosts = await this.profileService.getProfile()
+        this.profile.set(profilePosts)
     }
     
 
