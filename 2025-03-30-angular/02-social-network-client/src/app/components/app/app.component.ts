a509f20694c2de2866d40ebb923508b63d9f8f6d
shaharsol <shahar@gnift.com>
2025-03-30 10:29:28 +0300

    yoav

--- 2025-03-30-angular/02-social-network-client/src/app/components/app/app.component.ts
+++ 2025-03-30-angular/02-social-network-client/src/app/components/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LayoutComponent } from "../layout/layout/layout.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LayoutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
