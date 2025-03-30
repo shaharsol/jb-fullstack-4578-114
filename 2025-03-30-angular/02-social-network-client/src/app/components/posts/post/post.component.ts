2ac9d1a52f96aa8b20e7baa58a4331721b0e3172
shaharsol <shahar@gnift.com>
2025-03-30 15:22:53 +0300

    props

--- 2025-03-30-angular/02-social-network-client/src/app/components/posts/post/post.component.ts
+++ 2025-03-30-angular/02-social-network-client/src/app/components/posts/post/post.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { Post } from '../../../models/post/post.model';
 
 @Component({
   selector: 'app-post',
@@ -7,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './post.component.css'
 })
 export class PostComponent {
-
+    post = input<Post>()
 }
