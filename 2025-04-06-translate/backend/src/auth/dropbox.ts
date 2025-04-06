019df92e1a903fcbdf4062d46d8c89ab563c8c78
shaharsol <shahar@gnift.com>
2025-04-06 14:57:28 +0300

    alota

--- 2025-04-06-translate/backend/src/auth/dropbox.ts
+++ 2025-04-06-translate/backend/src/auth/dropbox.ts
@@ -1,9 +1,15 @@
-import passport from "passport";
+import passport, { DoneCallback } from "passport";
 import { Strategy } from 'passport-dropbox-oauth2'
 import config from 'config'
 
 const dropboxConfig = {...config.get<object>('dropbox')}
 
-passport.use(new Strategy(dropboxConfig, () => {}))
+passport.use(new Strategy(dropboxConfig, async function(accessToken: string, refreshToken: string, profile, done: DoneCallback) {
+    try {
+
+    } catch (e) {
+        
+    }
+}))
 
 export default passport
--- 2025-04-06-translate/backend/src/routers/dropbox.ts
+++ 2025-04-06-translate/backend/src/routers/dropbox.ts
@@ -7,6 +7,12 @@ import dropboxAuth from "../auth/dropbox";
 const dropboxRouter = Router()
 
 dropboxRouter.get('/auth', dropboxAuth.authenticate('dropbox-oauth2'))
-dropboxRouter.get('/callback', () => {})
+dropboxRouter.get('/callback', dropboxAuth.authenticate('dropbox-oauth2', {
+    session: false,
+    failureRedirect: 'http://localhost:5173/'
+}), function(req, res) {
+    // create jwt
+    res.redirect('http://localhost:5173/')
+})
 
 export default dropboxRouter
