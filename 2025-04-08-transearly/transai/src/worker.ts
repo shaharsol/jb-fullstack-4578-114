638ec05f8403dd1eb3a775245231167e266449b2
shaharsol <shahar@gnift.com>
2025-04-08 15:57:24 +0300

    uzbek work

--- 2025-04-08-transearly/transai/src/worker.ts
+++ 2025-04-08-transearly/transai/src/worker.ts
@@ -3,8 +3,9 @@ import axios from 'axios';
 import config from 'config'
 import ConvertAPI from 'convertapi';
 import OpenAI from 'openai';
-
-const convertapi = new ConvertAPI(config.get('convertApi.secret'));
+import { UserModel } from './models/user';
+import { Dropbox } from 'dropbox';
+import { connect } from './db/mongoose';
 
 const sqsConfig = JSON.parse(JSON.stringify(config.get('sqs.connection')))
 
@@ -16,6 +17,9 @@ const chatGpt = new OpenAI({
 });
 
 async function work() {
+
+    await connect()
+
     while(true) {
 
         try {
@@ -32,21 +36,31 @@ async function work() {
 
                 // get the text to translate
                 const axiosResponse = await axios(payload.link)
+                console.log(`translation is ${axiosResponse.data}`)
     
                 const response = await chatGpt.responses.create({
                     model: 'gpt-4o',
                     input: `please translate the following to ${payload.language}: ${axiosResponse.data}`,
                 });
-                  
+                
+                console.log(response)
 
 
+                console.log(`user id is ${payload.userId}`)
+                // now save the file in dropbox
+                const user = await UserModel.findById(payload.userId)
+                const dbx = new Dropbox({accessToken: user!.dropbox.accessToken})
 
+                await dbx.filesUpload({
+                    contents: response.output_text,
+                    path: `/test.${payload.language}.txt`
+                })
 
                 await sqsClient.send(new DeleteMessageCommand({
-                    QueueUrl: config.get<string>('sqs.docxToTxtQueueUrl'),
+                    QueueUrl: config.get<string>('sqs.translateQueueUrl'),
                     ReceiptHandle,
                 }))
-                console.log(`deleted message from docx-to-txt queue`)
+                console.log(`deleted message from translate queue`)
             }
     
     
